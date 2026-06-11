163760549e66beb05dfe0195557dbe0d2e1ac00c
Loyalcod <victorakujuobichibuike@gmail.com>
2026-06-11 17:10:50 +0100

    added the featured title and the properties and also the our recommendation section of the properties too

--- app/(root)/(tab)/index.tsx
+++ app/(root)/(tab)/index.tsx
@@ -1,9 +1,11 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import {
   Image,
   Text,
+  TouchableOpacity,
   View
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -16,6 +18,11 @@ export default function Index() {
   return (
     <>
       <SafeAreaView className="bg-white h-full">
+
+      {/* <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName='pb-32'
+      > */}
         <View className="px-5">
           <View className="flex flex-row items-center justify-between mt-5">
             <View className="flex flex-row items-center">
@@ -28,13 +35,47 @@ export default function Index() {
 
             <Image source={icons.bell} className="size-5" />
           </View>
-        </View>
 
-        <Search/>
+          <Search/>
 
         <View className="my-5">
-          <View className="flex flex-row items-center"></View>
+          <View className="flex flex-row items-center justify-between ">
+            <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
         </View>
+
+        <View className="flex flex-row gap-5">
+        <FeaturedCard/>
+        <FeaturedCard/>
+        </View>
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between ">
+            <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View className="flex flex-row gap-5">
+        <Card/>
+        <Card/>
+        </View>
+
+
+        
+        </View>
+
+        {/* </ScrollView> */}
+
+        
+
+
+        
       </SafeAreaView>
     </>
   );
